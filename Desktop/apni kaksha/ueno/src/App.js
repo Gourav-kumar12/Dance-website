f7d54c778ab68a2cd1c1a761e783fd4d26fbc8cd
Gourav <gk082522@gmail.com>
2021-08-16 00:06:10 +0530

    Initial commit

---
+++ Desktop/apni kaksha/ueno/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Header from './Header'
+import Mid from './Mid'
+
+export const App = () => {
+  return (
+    <>
+  <Navbar>
+  </Navbar>
+  <Header>
+  </Header>
+  <Mid>
+   </Mid>
+
+    </>
+  )
+}
+export default App;
